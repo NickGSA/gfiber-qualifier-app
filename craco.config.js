5a25b9c068033c5d1c96f9c4386b13f0f6fcc019
NickGSA <garcia.nicholas3006@gmail.com>
2025-05-28 11:53:51 -0500

    Fix ESLint react-hooks/exhaustive-deps warning for gfiberPlans

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,20 @@
-    module.exports = {
-      style: {
-        postcss: {
+// craco.config.js
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  style: {
+    postcss: {
+      mode: 'extends', // This tells CRACO to extend React-scripts' internal PostCSS config
+      loaderOptions: {
+        postcssOptions: {
+          ident: 'postcss',
           plugins: [
-            require('tailwindcss'),
-            require('autoprefixer'),
+            tailwindcss('./tailwind.config.js'), // Explicitly pass tailwind.config.js path
+            autoprefixer,
           ],
         },
       },
-    };
+    },
+  },
+};
