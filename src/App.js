8006cdc9c39a5039e428b4a49dcaa499d75b171b
NickGSA <garcia.nicholas3006@gmail.com>
2025-05-29 01:51:28 -0500

    new upload field version

--- src/App.js
+++ src/App.js
@@ -375,8 +375,8 @@ const App = () => {
               data={speedChartData}
               margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
             >
-              {/* Adjusted offset to 10 */}
-              <XAxis type="number" label={{ value: 'Speed (Mbps)', angle: 0, position: 'bottom', offset: 10 }} />
+              {/* Adjusted offset to 5 */}
+              <XAxis type="number" label={{ value: 'Speed (Mbps)', angle: 0, position: 'bottom', offset: 5 }} />
               <YAxis type="category" dataKey="category" />
               <Tooltip formatter={(value) => `${value.toLocaleString()} Mbps`} />
               <Legend wrapperStyle={{ paddingTop: 20 }} />
